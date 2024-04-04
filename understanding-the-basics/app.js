6ffe78953584a9b8811455ad119cdf2eea933817
Numan Şenlik <nmnsnlk@gmail.com>
2024-04-04 04:02:32 +0300

    create message folder for users message

--- understanding-the-basics/app.js
+++ understanding-the-basics/app.js
@@ -1,7 +1,29 @@
 const http = require("http");
-
+const fs = require("fs");
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body>");
+    res.write("<form  action='/message' method='POST'>");
+    res.write(
+      "<input name='message' type=text placeholder='Bir Mesaj Giriniz'></input>"
+    );
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  res.write("<button type='submit' > Mesaji gonder</button>");
+  res.write("</form>");
+  res.write("</body>");
+  res.write("</html>");
+  return res.end();
 });
 
 server.listen(3000);
